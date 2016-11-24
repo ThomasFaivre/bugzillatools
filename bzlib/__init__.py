81d8bf897c9413fc081b196630b26d3abc9d4bae
Thomas Faivre <thomas.faivre@6wind.com>
2016-11-24 13:29:54 +0100

    bzlib: Add a list of public module
    Only add the bug, bugzilla and config modules as they are the most used.
    
    Signed-off-by: Thomas Faivre <thomas.faivre@6wind.com>

--- bzlib/__init__.py
+++ bzlib/__init__.py
@@ -15,6 +15,19 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from . import (
+    bug,
+    bugzilla,
+    config,
+)
+
+__all__ = [
+    'bug',
+    'bugzilla',
+    'config',
+]
+
+
 version_info = (0, 6, 0, 'final', 0)
 
 version_fmt = '{0}.{1}'
